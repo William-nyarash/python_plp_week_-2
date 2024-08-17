5442498663d46f79f7b1aac8105e089af4046e25
william nyarash <114805151+William-nyarash@users.noreply.github.com>
2024-08-17 17:11:53 +0300

    add list implimentation

---
+++ working_with_list.py
@@ -0,0 +1,24 @@
+
+my_list = []
+
+# append the elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#insert the value 15 at the second position
+my_list.insert(1,15)
+
+# extend the list with another
+
+my_list.extend([50,60,70])
+
+# Remove the last element from my_list
+my_list.pop()
+
+# Sort my_list in ascending order
+my_list.sort()
+
+# Find and print the index of the value 30 in my_list
+print(my_list.index(30))
